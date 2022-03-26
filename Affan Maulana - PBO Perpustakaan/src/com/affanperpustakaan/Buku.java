a206f45bb5d003fcacf0a30eb5f14d217ba74f28
affan maul <92240068+affanmaul@users.noreply.github.com>
2022-03-26 21:58:39 +0700

    Add files via upload

---
+++ Affan Maulana - PBO Perpustakaan/src/com/affanperpustakaan/Buku.java
@@ -0,0 +1,16 @@
+package com.affanperpustakaan;
+
+// Kelas Buku
+public class Buku{
+    // Atribut
+    String judulBuku;
+    String penulis;
+    String tebalBuku;
+
+    // Constructor
+    public Buku(String judulBuku, String penulis, String tebalBuku) {
+        this.judulBuku = judulBuku;
+        this.penulis = penulis;
+        this.tebalBuku = tebalBuku;
+    }
+}
